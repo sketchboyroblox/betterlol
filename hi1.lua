f50318961a6699dcc89bb49b2456c49a9a2174bd
sketchboyroblox <raywilliamsaa@gmail.com>
2025-12-22 18:24:28 -0500

    Update hi1.lua

--- hi1.lua
+++ hi1.lua
@@ -235,7 +235,7 @@ local function createModernUI()
     vanityStroke.Parent = vanityInput
 
     vanityInput.FocusLost:Connect(function()
-        discordVanity = vanityInput.Text ~= "" and vanityInput.Text or "/chrono"
+        discordVanity = vanityInput.Text ~= "" and vanityInput.Text or "/weep"
         pcall(function()
             if initializeMessageVariations then
                 initializeMessageVariations()
@@ -1364,6 +1364,7 @@ local function initialize()
 end
 
 initialize()
+
 
 
 
